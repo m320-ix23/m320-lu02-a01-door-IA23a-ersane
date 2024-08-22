804216008bfde8cf36bc4e01ea370c041be25001
Eylül Ersan <eyluel.ersan@bbcag.ch>
2024-08-22 10:55:35 +0200

    verbesserungen

--- door.py
+++ door.py
@@ -8,7 +8,8 @@ class Door:
     """
 
     # Mit dem Keyword def wird eine Funktion bzw. eben ein Konstruktor deklariert.
-    # Der Konstruktor trägt IMMER den Namen __init__ und weist als ersten Parameter den Wert self auf.
+    # Der Konstruktor trägt IMMER den Namen __init__ und weist als ersten
+    # Parameter den Wert self auf.
     # Danach folgen die Übergabeparameter, deren Werte dann den Attributen zugewiesen werden.
     # Attribute können aber auch mit einem fixen Wert initialisiert werden.
     # Konstruktoren werden als Erstes im Programm angeschrieben.
@@ -20,6 +21,7 @@ class Door:
         """
         # ein privates Attribut muss im Konstruktor initialisiert werden und ist dann in der Klasse
         # über self._name_des_Attributs ansprechbar.
+        self.door_is_locked = None
         self._the_door_lock = ref2door_lock
         # Hier wird der Setter eines Attributs aufgerufen (siehe unten)
         self.color = base_color
@@ -33,13 +35,14 @@ class Door:
         Methode für das öffnen der Türe.
         Das ist aber nur möglich, wenn die Türe nicht verriegelt ist.
         """
-        if self._door_is_locked == False:
+        if not self._door_is_locked:
             self._door_is_open = True
 
     def close_the_door(self):
         """
         Methode für das schliessen der Türe.
-        Das geht immer, auch wenn die Türe schon geschlossen oder verriegelt ist. Der Zustand ändert dann nämlich nicht.
+        Das geht immer, auch wenn die Türe schon geschlossen
+        oder verriegelt ist. Der Zustand ändert dann nämlich nicht.
         """
         self._door_is_open = False
 
@@ -49,7 +52,7 @@ class Door:
         Das ist nur möglich, wenn die Türe nicht offen ist.
         Für das verriegeln ist aber das Türschloss zuständig. Es weiss wie das geht.
         """
-        if self._door_is_open == False:
+        if not self._door_is_open:
             self._door_is_locked = self._the_door_lock.lock()
 
     def unlock_the_door(self):
@@ -105,9 +108,20 @@ class Door:
         self._color = new_color
 
 
-"""
-nur für die korrekte Übersetzung und Ausführung 
-"""
+def unlock(self):
+    """
+           Methode zum Verriegeln des Schlosses.
+           :return: True, um anzuzeigen, dass das Schloss verriegelt ist.
+           """
+    return False
+
+
+def lock(self):
+    """
+              Methode zum Verriegeln des Schlosses.
+              :return: True, um anzuzeigen, dass das Schloss verriegelt ist.
+              """
+    return True
 
 
 class DoorLock:
@@ -117,13 +131,7 @@ class DoorLock:
 
     def __init__(self):
         print("ein Schloss erzeugt")
-
-    def lock(self):
-        return True
-
-    def unlock(self):
-        return False
-
+        self.door_is_locked = None
 
 # Hier die main-Methode festlegen
 if __name__ == "__main__":
